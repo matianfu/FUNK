19cd9a4f16c9fb4716bd8a06ac12ed9a26cd5502
matianfu <matianfu@gmail.com>
2015-04-15 02:23:46 +0800

    clean c++ file. only C version in future.

--- inc/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COMMON_H_
-#define __COMMON_H_
-
-#define CONCAT01(s1, s2)    s1##s2
-#define CONCAT02(s1, s2)    CONCAT01(s1, s2)
-
-#endif
-
-
--- src/main.c
+++ src/main.c
@@ -18,9 +18,9 @@ Continuation* add(Continuation* co,
   struct timespec start;
   VAR_END
 
-  if (!this) { *ret = 1; EXIT(); }
+  if (!this) { *ret = -1; EXIT(); }
 
-  clock_gettime(CLOCK_REALTIME_COARSE, &this->start);
+  clock_gettime(CLOCK_REALTIME, &this->start);
 
   while(1)
   {
